c3f04c2dee9a87875997fcfd6e2171172f78a69e
dbaines <dbaines@gmail.com>
2018-06-22 16:08:16 +0930

    Moved multiple rollup configs in to single file

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,46 @@
-import config from './rollup.config.shared';
+// rollup.config.js
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import json from 'rollup-plugin-json';
+import minify from 'rollup-plugin-babel-minify';
 
-export default config;
+const inputFile = './src/show-if.js';
+const defaultPlugins = [
+  resolve(),
+  json(),
+  babel({
+    exclude: 'node_modules/**' // only transpile our source code
+  })
+]
+
+export default [
+
+  // Regular UMD Build
+  {
+    input: inputFile,
+    output: {
+      file: './dist/show-if.umd.js',
+      format: 'umd'
+    },
+    plugins: defaultPlugins,
+  },
+
+  // Minified UMD Build
+  {
+    input: inputFile,
+    output: {
+      file: './dist/show-if.umd.min.js',
+      format: 'umd',
+    },
+    plugins: [
+      ...defaultPlugins,
+      minify({
+        comments: false,
+        sourceMap: false,
+        bannerNewLine: true,
+        banner: "/* ShowIf.js */",
+      })
+    ]
+  }
+
+];
