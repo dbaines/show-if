c20bb0f85246f23f44636614b6470f41388946d5
dbaines <dbaines@gmail.com>
2018-06-22 15:42:09 +0930

    Added minified rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,3 @@
-// rollup.config.js
-import resolve from 'rollup-plugin-node-resolve';
-import babel from 'rollup-plugin-babel';
-import json from 'rollup-plugin-json';
+import config from './rollup.config.shared';
 
-export default {
-  input: './src/show-if.js',
-  output: {
-    file: './dist/show-if.umd.js',
-    format: 'umd'
-  },
-  plugins: [
-    resolve(),
-    json(),
-    babel({
-      exclude: 'node_modules/**' // only transpile our source code
-    }),
-  ]
-};
+export default config;
